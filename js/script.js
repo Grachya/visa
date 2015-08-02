e82db949ace368a17dd61c6ede452f061bf39f4c
Grachya <grach41@gmail.com>
2015-08-02 10:30:24 +0300

    ready fo 70%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	$("#phone").mask("(999) 999-9999");
+	// input mask
+	$("#phone").mask("+7(999)999-99-99");
+
+	// service p height
+	var pheight = $('.ser p');
+	var serParray = [];
+	for(var i=0; i<pheight.length; i++){
+		serParray.push($(pheight[i]).height()); 
+	}
+	var finalPheight = serParray.reduce(function(x,y){return (x>y)?x:y;});
+	$('.ser p').css({'height':finalPheight});
+
+	// next
 })
