6dd916ec0693e5746933dcfc288320ddad024ec1
Grachya <grach41@gmail.com>
2015-08-19 22:05:53 +0300

    new design

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	// input mask
+	$("#phone").mask("+7(999)999-99-99");
+	$("#footerPhone").mask("+7(999)999-99-99");
+	
+
+	// service p height
+	var pheight = $('.ser p');
+	var serParray = [];
+	for(var i=0; i<pheight.length; i++){
+		serParray.push($(pheight[i]).height()); 
+	}
+	var finalPheight = serParray.reduce(function(x,y){return (x>y)?x:y;});
+	$('.ser p').css({'height':finalPheight});
+
+})
